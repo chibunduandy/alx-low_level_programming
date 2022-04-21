6195ae7136636521ded824d9c9cdd77860d8b47d
Chibundu <andybundie@gmail.com>
2022-04-20 17:09:47 -0700

    Swapping values of integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*swap_int - prints all natural numbers from n to 98
+*@a: pointer to the first number
+*@b: pointer to the second number
+* Return: returns 0
+*/
+void swap_int(int *a, int *b)
+
+{
+int temp;
+temp = *a;
+*a = *b;
+*b = temp;
+}
