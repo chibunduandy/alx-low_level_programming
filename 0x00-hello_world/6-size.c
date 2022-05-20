af4e50842ffa7cefd99872d3af5dc99b2453b7f4
Chibundu <andybundie@gmail.com>
2022-05-19 17:51:05 -0700

    C project commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,17 +2,15 @@
 
 /**
  * main - Entry point
- * Return: Always 0 (success)
- *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("Size of a char: %zu bytes\n", sizeof(char));
-	printf("Size of a long: %zu bytes\n", sizeof(long));
-	printf("Size of a unsigned long: %zu bytes\n", sizeof(unsigned long));
-	printf("Size of a double: %zu bytes\n", sizeof(double));
-	printf("Size of a float: %zu bytes\n", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-		return (0);
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
